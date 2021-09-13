a2b46e9baa6cab7d46b1c08ed9627470aded5d71
Arjun Doel <arjundoel@gmail.com>
2021-09-13 14:41:06 +0100

    collected data

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 angular.module('apiProject', [])
-  .controller('apiController', function($scope, $http) {
+  .controller('apiController', function ($scope, $http) {
 
-    $scope.header = 'Angular API Project';
+    $scope.header = 'Angular API Project'
 
-    $http.get('www.themealdb.com/api/json/v1/1/list.php?c=list')
+    $http.get('https://www.themealdb.com/api/json/v1/1/search.php?s=chicken').then(successCallback, errorCallback);
 
-  })
+    function successCallback(response) {
+      $scope.meals = response.data.meals;
+      console.log('data', $scope.meals);
+    }
+    function errorCallback(error) {
+      console.log(error)
+    }
+
+  });
