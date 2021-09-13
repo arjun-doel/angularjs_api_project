d26db4bc8c894373e99544bc49d69a38f50d5211
Arjun Doel <arjundoel@gmail.com>
2021-09-13 15:18:40 +0100

    hot reload added

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ angular.module('apiProject', [])
 
     $scope.header = 'Angular API Project'
 
-    $http.get('https://www.themealdb.com/api/json/v1/1/search.php?s=chicken').then(successCallback, errorCallback);
+    $scope.mealSearch = function(event) {
+      $scope.userSearch = event.target.value;
+      console.log($scope.userSearch)
+      $http.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${$scope.userSearch}`).then(successCallback, errorCallback);
+    }
+
+    
 
     function successCallback(response) {
       $scope.meals = response.data.meals;
